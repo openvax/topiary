ee4881f5a781daeccc04bcb555ce8db6c7d8d871
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-04-07 12:44:51 -0400

    Skip NetMHC tests gracefully when binaries are not installed
    Wrap NetMHCpan/NetMHCIIpan initialization in try/except and use
    pytestmark to skip all tests in the module when the tools aren't
    available. Tests still run fully in CI where netmhc-bundle is present.

--- tests/test_mutant_epitope_predictions_class2.py
+++ tests/test_mutant_epitope_predictions_class2.py
@@ -10,14 +10,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from mhctools import NetMHCIIpan
-
+import pytest
 from pyensembl import ensembl_grch37
 from topiary import TopiaryPredictor
 from varcode import Variant, VariantCollection
 
 from .common import eq_
+
+try:
+    from mhctools import NetMHCIIpan
+
+    mhc_model = NetMHCIIpan(
+        alleles=["HLA-DPA1*01:05/DPB1*100:01", "DRB10102"],
+        default_peptide_lengths=[15, 16],
+    )
+    HAS_NETMHC = True
+except Exception:
+    mhc_model = None
+    HAS_NETMHC = False
+
+pytestmark = pytest.mark.skipif(not HAS_NETMHC, reason="NetMHCIIpan not installed")
 
 # TODO: find out about these variants,
 # what do we expect from them? Are they SNVs?
@@ -27,10 +39,6 @@ variants = VariantCollection(
         Variant(contig=11, start=32861682, ref="G", alt="A", ensembl=ensembl_grch37),
     ]
 )
-
-alleles = ["HLA-DPA1*01:05/DPB1*100:01", "DRB10102"]
-
-mhc_model = NetMHCIIpan(alleles=alleles, default_peptide_lengths=[15, 16])
 
 
 def test_netmhcii_pan_epitopes():
